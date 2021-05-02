dc42679f453b4892a482c27c63b874c9f75be4ea
NALLASAMY <NALLASAMY@LAPTOP-JCE6F948>
2021-05-02 19:04:36 +0530

    pr

--- src/com/nsgglobal/test/HashMapClass.java
+++ src/com/nsgglobal/test/HashMapClass.java
@@ -7,7 +7,7 @@ public class HashMapClass {
 	public static void main(String args[]) {
 		//oooooo
 	HashMap<String, Integer> hmapListObj = new HashMap<>(); //pppppppppppppppppp
-	hmapListObj.put("Hello",10);
+	hmapListObj.put("Hello",10); //oioi
 	hmapListObj.put("Hello", 30);
 	
 	System.out.println("hmapListObj:\t" + hmapListObj.get("Hello"));
