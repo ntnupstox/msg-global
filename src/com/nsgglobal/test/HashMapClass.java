8230799a3bca84864a1a55b39a04be4019e502ae
NALLASAMY <NALLASAMY@LAPTOP-JCE6F948>
2021-05-02 18:36:13 +0530

    second cjange

--- src/com/nsgglobal/test/HashMapClass.java
+++ src/com/nsgglobal/test/HashMapClass.java
@@ -7,7 +7,7 @@ public class HashMapClass {
 	public static void main(String args[]) {
 		//oooooo
 	HashMap<String, Integer> hmapListObj = new HashMap<>(); //pppppppppppppppppp
-	hmapListObj.put("Hello",10);
+	hmapListObj.put("Hello",20);
 	hmapListObj.put("Hello", 40);
 	
 	System.out.println("hmapListObj:\t" + hmapListObj.get("Hello"));
