659da98151a19815b49b26c7f319cc2b87fb9cb5
NALLASAMY <NALLASAMY@LAPTOP-JCE6F948>
2021-05-02 12:28:39 +0530

    only commit

--- src/com/nsgglobal/test/HashMapClass.java
+++ src/com/nsgglobal/test/HashMapClass.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class HashMapClass {
 	
 	public static void main(String args[]) {
-		
+		//oooooo
 	HashMap<String, Integer> hmapListObj = new HashMap<>();
 	hmapListObj.put("Hello",10);
 	hmapListObj.put("Hello", 20);
